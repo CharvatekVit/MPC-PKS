0bd10351ec2b6303c1a9c9fe717c90e4fc33c2f0
CharvatekVit <246850@vutbr.cz>
2026-02-17 21:28:21 +0100

    task 3

--- PC1/task03.py
+++ PC1/task03.py
@@ -8,7 +8,7 @@ port = 50000
 udp_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 #zaslani zpravy
 for i in range(5):
-    data = struct.pack("!LLL", i, 221342, 81)
+    data = struct.pack("!LLL", i, 246850, 00)
     n = udp_socket.sendto(data,(host, port))     
     print("Odeslano {} byte\n".format(n))
     try:
